fef92633c7ac1ec920f915d79e8049b11484b205
six-shot <okhuomonajayi54@gmail.com>
2024-02-06 15:08:54 -0800

    commit

--- app/components/Enjoy.js
+++ app/components/Enjoy.js
@@ -1,12 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import enjoy from "@/public/enjoy.png";
-
+import line from "@/public/line.png";
 export default function Enjoy() {
   return (
     <section className="px-[6%] -mt-[20%]">
       <div>
-        <Image src={enjoy} alt="enjoying the view" className="w-full" />
+        <Image
+          src={enjoy}
+          alt="enjoying the view"
+          className="w-full shadow-[0_75px_100px_-50px_#38425580]"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center mt-14">
+        <Image src={line} alt="enjoying the view" />
+        <h4 className="font-league font-bold text-[28px] mt-8">
+          Enjoyable place for
+        </h4>
+        <p className="text-center font-league">
+          Our relaxed surroundings make dining with us a great experience for
+          everyone. We can even arrange a tour of the farm before your meal.
+        </p>
       </div>
     </section>
   );
