8e399447bb428ec577321b1ff0cae2251d4feb35
six-shot <okhuomonajayi54@gmail.com>
2024-02-08 01:48:09 -0800

    commit

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -73,6 +73,30 @@ export default function Footer() {
           </h5>
         </div>
       </div>
+      <div className="sm:flex hidden">
+        <div
+          style={{
+            // use the src property of the image object
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${foot.src})`,
+            // other styles
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "240px",
+           
+          }}
+        >
+          <div className="flex justify-between items-center h-[240px] px-[11.5%] ">
+            <h4 className="font-league font-bold text-[48px] text-center text-white mb-2 mt-8">
+              Ready to make a reservation?
+            </h4>
+            <button className="uppercase font-league mt-1 w-[245px] h-[60px] flex justify-center items-center border border-white text-white font-semibold text-base tracking-[2.5px]">
+              book a table
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
