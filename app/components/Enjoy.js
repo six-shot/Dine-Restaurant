0bece7f6895034b39946f0d024dff929d34985f9
six-shot <okhuomonajayi54@gmail.com>
2024-02-07 22:10:04 -0800

    commit

--- app/components/Enjoy.js
+++ app/components/Enjoy.js
@@ -51,7 +51,7 @@ export default function Enjoy() {
           </div>
         </div>
       </div>
-      <div className="sm:flex hidden">
+      <div className="sm:flex flex-col hidden">
         <div className="grid grid-cols-12 w-full py-[10%]">
           <div className="col-span-7 bg-[#f2f3f5] w-full h-[320px] rounded-tr-[100px] relative">
             <Image
@@ -61,6 +61,27 @@ export default function Enjoy() {
             />
           </div>
           <div className="col-span-5 w-full relative -top-[22%] -left-[12%]">
+            <Image src={line} alt="enjoying the view" />
+            <h4 className="font-league font-bold text-[48px]  leading-[48px] text-[#242B37] mb-4 mt-14">
+              Enjoyable place <br /> for all the family
+            </h4>
+            <p className=" font-league text-[#242B37] font-normal text-[20px] w-[445px]">
+              Our relaxed surroundings make dining with us a great experience
+              for everyone. We can even arrange a tour of the farm before your
+              meal.
+            </p>
+          </div>
+        </div>
+        
+        <div className="grid grid-cols-12 w-full py-[30%] pr-[11.5%]">
+          <div className="col-span-7 order-last grid justify-end bg-[#f2f3f5] w-full h-[320px] rounded-tl-[100px] relative">
+            <Image
+              src={enjoyhero}
+              alt="enjoying the view"
+              className=" -mt-[30%] shadow-[0_75px_100px_-50px_#38425580]"
+            />
+          </div>
+          <div className="col-span-5 w-full relative ">
             <Image src={line} alt="enjoying the view" />
             <h4 className="font-league font-bold text-[48px]  leading-[48px] text-[#242B37] mb-4 mt-14">
               Enjoyable place <br /> for all the family
